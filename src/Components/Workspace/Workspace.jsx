acd625b94942b86e7b78be6c57896dd5e442b057
farkh <farkhutdinov1398@gmail.com>
2019-05-29 21:40:49 +0300

    Add workspace, header, footer components

--- src/Components/Workspace/Workspace.jsx
+++ src/Components/Workspace/Workspace.jsx
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
 import './scss/_workspace.scss';
 
 class Workspace extends Component {
 	render() {
 		return (
 			<div className="workspace">
-				<h1>Hello world</h1>
+				<Header />
+				
+				{this.props.children}
+				
+				<Footer />
 			</div>
 		);
 	}
