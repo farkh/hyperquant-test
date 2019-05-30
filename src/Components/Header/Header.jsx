6f6a123e83707983d9b5699c01da7ea376a58b99
farkh <farkhutdinov1398@gmail.com>
2019-05-30 21:01:59 +0300

    Header done

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,8 +5,12 @@ import './scss/_header.scss';
 
 const Header = () => {
 	return (
-		<header>
+		<header className="header">
+			<div className="header__menu-icon" />
+			
 			<p className="header__title">Dashboard</p>
+
+			<div className="header__refresh-icon" />
 		</header>
 	);
 };
