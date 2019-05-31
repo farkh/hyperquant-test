66df512c0cef147d4aef5af9f6d6c2d54caf3f0a
farkh <farkhutdinov1398@gmail.com>
2019-05-31 23:12:40 +0300

    Add data

--- src/Components/TimeRange/RangeItem.jsx
+++ src/Components/TimeRange/RangeItem.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const RangeItem = () => {
+const RangeItem = ({ value, text, isActive, onRangeClick }) => {
 	return (
-		<div className="time-range__range">
-			All time
+		<div
+			className={`time-range__range ${isActive && 'time-range__range--active'}`}
+			onClick={() => onRangeClick(value)}
+		>
+			{text}
 		</div>
 	);
+};
+
+RangeItem.propTypes = {
+	value: PropTypes.number.isRequired,
+	text: PropTypes.string.isRequired,
+	isActive: PropTypes.bool,
+	onRangeClick: PropTypes.func.isRequired,
+};
+
+RangeItem.defaultProps = {
+	isActive: false,
 };
 
 export default RangeItem;
--- src/Components/TimeRange/TimeRange.jsx
+++ src/Components/TimeRange/TimeRange.jsx
@@ -4,17 +4,50 @@ import RangeItem from './RangeItem';
 
 import './scss/_time-range.scss';
 
+const RANGES = [
+	{
+		value: 1,
+		text: '24h',
+	},
+	{
+		value: 7,
+		text: '7 days',
+	},
+	{
+		value: 30,
+		text: '30 days',
+	},
+	{
+		value: 0,
+		text: 'All time',
+	},
+];
+
 class TimeRange extends Component {
+	state = {
+		selectedValue: 0,
+	};
+
+	handleRangeItemClick = (value) => {
+		this.setState({ selectedValue: value });
+	};
+	
 	render() {
+		const { selectedValue } = this.state;
+		
 		return (
 			<div className="dashboard__time-range time-range">
 				<p className="time-range__label">Time Range:</p>
 
 				<div className="time-range__ranges">
-					<RangeItem />
-					<RangeItem />
-					<RangeItem />
-					<RangeItem />
+					{RANGES && RANGES.map(range => (
+						<RangeItem
+							key={range.value}
+							{...range}
+							isActive={selectedValue === range.value}
+							onRangeClick={this.handleRangeItemClick}
+						/>
+					))}
 				</div>
 			</div>
 		)
